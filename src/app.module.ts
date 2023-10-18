03ddafe6ecab5922d7aba736b124c162b0bc54ec
Acrata <hxtr227@gmail.com>
2023-10-18 03:47:13 +0200

    MongoDb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://skin:O2K1xfwR5hOhdq7u@skincraft.rl0qanz.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
